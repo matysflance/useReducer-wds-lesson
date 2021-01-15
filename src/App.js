d90137679b9fd20b05949282fafac646ab72bcfb
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-01-15 19:53:01 +0000

    2 - Basic usage of useReducer - no actions specified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useReducer } from 'react';
+import { useReducer } from 'react';
 
 /*
   useReducer also allows to modify state and rerender components when it changes
@@ -7,17 +7,31 @@ import { useState, useReducer } from 'react';
   It converts current state into new state based on actions we send it
 */
 
-function App() {
-  const [count, setCount] = useState(0);
+const reducer = (state, action) => {
+  // reducer function takes 2 parameters
+  // 1) current state
+  // 2) action - whatever we pass into dispatch() function
+  return { count: state.count + 1 };
+};
 
-  const increment = () => setCount((prevCount) => prevCount + 1);
-  const decrement = () => setCount((prevCount) => prevCount - 1);
+function App() {
+  // useReducer takes 2 parameters
+  // 1) Function we perform on our state to get the new state
+  // 2) Initial value
+
+  // and returns:
+  // 1) state
+  // 2) dispatch function - used to call reducer function based on certain parameters (look line 10)
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  const increment = () => dispatch();
+  // const decrement = () => setCount((prevCount) => prevCount - 1);
 
   return (
     <div className="App">
       <h1>Learnign useReducer with WDS</h1>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
+      {/* <button onClick={decrement}>-</button> */}
+      <span>{state.count}</span>
       <button onClick={increment}>+</button>
     </div>
   );
