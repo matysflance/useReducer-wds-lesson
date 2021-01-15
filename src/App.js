f5dec923a08e5e0e0252b02e3984992c74950441
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-01-15 18:52:45 +0000

    Counter with simple useState for demonstration purposes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((prevCount) => prevCount + 1);
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+
   return (
     <div className="App">
       <h1>Learnign useReducer with WDS</h1>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
